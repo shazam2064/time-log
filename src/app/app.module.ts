68cd240bd97e15ab89f06eb4a058a704e0996fff
gabriel <gabriel.salomon.2007@gmail.com>
2019-12-15 16:59:03 -0500

    I started a new project.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule
