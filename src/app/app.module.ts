a8bb3ffd35f06013f36b58c0b73328895be44db8
gabriel <gabriel.salomon.2007@gmail.com>
2019-12-19 06:24:01 -0500

    This shall be reverted.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserListComponent } from './user-list/user-list.component';
-import {UserListService} from './user-list/user-list.service';
-import {UserEditComponent} from './user-list/user-edit/user-edit.component';
+import { UserListService } from './user-list/user-list.service';
+import { UserEditComponent } from './user-list/user-edit/user-edit.component';
+import { ProjectComponent } from './project/project.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     UserListComponent,
-    UserEditComponent
+    UserEditComponent,
+    ProjectComponent,
+    UserlistComponent
   ],
   imports: [
     BrowserModule
--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 
-import { Ingredient } from '../shared/user.model';
-import { ShoppingListService } from './user-list.service';
+import { User } from './user.model';
+import { UserListService } from './user-list.service';
 
 @Component({
   selector: 'app-user-list',
@@ -10,14 +10,14 @@ import { ShoppingListService } from './user-list.service';
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent implements OnInit, OnDestroy {
-  users: Users[];
+  users: User[];
   private subscription: Subscription;
 
   constructor(private usService: UserListService) { }
 
   ngOnInit() {
     this.users = this.usService.getUsers();
-    this.subscription = this.usService.ingredientsChanged
+    this.subscription = this.usService.usersChanged
       .subscribe(
         (ingredients: User[]) => {
           this.users = ingredients;
