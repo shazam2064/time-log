b69b328d2e1da85c2f08088993e899c878edfa17
gabriel <gabriel.salomon.2007@gmail.com>
2019-12-21 10:40:24 -0500

    I added the project files. This is from yesterday

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,36 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-// import { ProjectComponent } from './project/project.component';
+import { ProjectComponent } from './project/project.component';
 import { UserListComponent } from './user-list/user-list.component';
-// import { ProjectStartComponent } from './project/project-start/recipe-start.component';
-// import { ProjectDetailComponent } from './project/project-detail/project-detail.component';
-// import { ProjectEditComponent } from './project/project-edit/project-edit.component';
+import { ProjectStartComponent } from './project/project-start/project-start.component';
+import { ProjectDetailComponent } from './project/project-detail/project-detail.component';
+import { ProjectEditComponent } from './project/project-edit/project-edit.component';
 // import { ProjectResolverService } from './project/project-resolver.service';
 // import { AuthComponent } from './auth/auth.component';
 // import { AuthGuard } from './auth/auth.guard';
 
 const appRoutes: Routes = [
-  // { path: '', redirectTo: '/project', pathMatch: 'full' },
-  // {
-  //   path: 'project',
-  //   component: ProjectComponent,
-  //   canActivate: [AuthGuard],
-  //   children: [
-  //     { path: '', component: ProjectStartComponent },
-  //     { path: 'new', component: ProjectEditComponent },
-  //     {
-  //       path: ':id',
-  //       component: ProjectDetailComponent,
-  //       resolve: [ProjectResolverService]
-  //     },
-  //     {
-  //       path: ':id/edit',
-  //       component: ProjectEditComponent,
-  //       resolve: [ProjectResolverService]
-  //     }
-  //   ]
-  // },
+  { path: '', redirectTo: '/project', pathMatch: 'full' },
+  {
+    path: 'project',
+    component: ProjectComponent,
+    // canActivate: [AuthGuard],
+    children: [
+      { path: '', component: ProjectStartComponent },
+      { path: 'new', component: ProjectEditComponent },
+      {
+        path: ':id',
+        component: ProjectDetailComponent,
+        // resolve: [ProjectResolverService]
+      },
+      {
+        path: ':id/edit',
+        component: ProjectEditComponent,
+        // resolve: [ProjectResolverService]
+      }
+    ]
+  },
   { path: 'user-list', component: UserListComponent },
   // { path: 'auth', component: AuthComponent }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserListComponent } from './user-list/user-list.component';
-import {UserListService} from './user-list/user-list.service';
-import {UserEditComponent} from './user-list/user-edit/user-edit.component';
+import { UserListService } from './user-list/user-list.service';
+import { UserEditComponent } from './user-list/user-edit/user-edit.component';
 import { ProjectComponent } from './project/project.component';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { ProjectDetailComponent } from './project/project-detail/project-detail.component';
+import { ProjectEditComponent } from './project/project-edit/project-edit.component';
+import { ProjectListComponent } from './project/project-list/project-list.component';
+import { ProjectStartComponent } from './project/project-start/project-start.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +19,11 @@ import {FormsModule} from '@angular/forms';
     HeaderComponent,
     UserListComponent,
     UserEditComponent,
-    ProjectComponent
+    ProjectComponent,
+    ProjectDetailComponent,
+    ProjectEditComponent,
+    ProjectListComponent,
+    ProjectStartComponent
   ],
   imports: [
     BrowserModule,
